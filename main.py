ec360287eb361797045203739f44eb391dd62ae5
ferhsadzadeee <ferhadzadenigar@gmail.com>
2025-05-24 10:10:04 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-#A+B?
-nums=input()
-a=int(nums[0])
-b=int(nums[2])
-print(a+b)
+#Əksəriyyət Elementini Tapın
+a = int(input(""))
+b = []
+while a!=0:
+	c = input("")
+	b.append(c)
+	a = a - 1
+h = len(b)
+s = 0
+for i in range(h):
+	k = b[i]
+	e = b.count(k)
+	if s<e:
+		s = e
+		d = k
+	else:
+		continue
+t = h/2
+if s > t:
+	print(d)
+else:
+	print("None")
